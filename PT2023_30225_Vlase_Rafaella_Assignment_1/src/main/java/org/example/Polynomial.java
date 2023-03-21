6344263e36308142ade48da48c4b154fe163d555
Vlase Rafaella <kmyelly2@gmail.com>
2023-03-21 15:37:32 +0200

    update class

--- PT2023_30225_Vlase_Rafaella_Assignment_1/src/main/java/org/example/Polynomial.java
+++ PT2023_30225_Vlase_Rafaella_Assignment_1/src/main/java/org/example/Polynomial.java
@@ -1,45 +1,69 @@
 package org.example;
 import java.util.Map;
 import java.util.TreeMap;
-public class Polynomial
-{
-    private static Map<Integer, Monomial> monomials;
-    public Polynomial()
-    {
+
+public class Polynomial {
+    private Map<Integer, Double> monomials;
+
+    public Polynomial() {
         monomials = new TreeMap<>();
     }
-    public Polynomial(Map<Integer, Monomial> monomials)
-    {
+
+    public Polynomial(Map<Integer, Double> monomials) {
         this.monomials = new TreeMap<>(monomials);
     }
 
-    public void addMonomial(Monomial monomial)
-    {
-        int degree = monomial.getDegree();
-        if(monomials.containsKey(degree))
-        {
-            Monomial existing = monomials.get(degree);
-            Monomial newMonomial = existing.addition(existing, monomial);
-            monomials.put(degree, newMonomial);
-        }
-        else
+    public void addDouble(int degree, Double monomial) {
+        if (monomials.containsKey(degree)) {
+            Double existing = monomials.get(degree);
+            Double newDouble = existing + monomial;
+            monomials.put(degree, newDouble);
+        } else {
             monomials.put(degree, monomial);
+        }
     }
-    public static Polynomial addition(Polynomial x)
-    {
-        Polynomial y = new Polynomial();
-        for(int degree : monomials.keySet())
-        {
-            Monomial monomial = monomials.get(degree);
-            y.addMonomial(monomial);
+
+    public static Polynomial addition(Polynomial x, Polynomial y) {
+        Polynomial result = new Polynomial();
+        for (int degree : x.monomials.keySet()) {
+            Double monomial = x.monomials.get(degree);
+            result.addDouble(degree, monomial);
         }
 
-        for(int degree : x.monomials.keySet())
-        {
-            Monomial monomial = x.monomials.get(degree);
-            y.addMonomial(monomial);
+        for (int degree : y.monomials.keySet()) {
+            Double monomial = y.monomials.get(degree);
+            result.addDouble(degree, monomial);
         }
-        return y;
+        return result;
     }
+
+    public static Polynomial subtraction(Polynomial x, Polynomial y) {
+        Polynomial result = new Polynomial();
+        for (int degree : x.monomials.keySet()) {
+            Double monomial = x.monomials.get(degree);
+            result.addDouble(degree, monomial);
+        }
+
+        for (int degree : y.monomials.keySet()) {
+            Double monomial = y.monomials.get(degree);
+            result.addDouble(degree, -monomial);
+        }
+        return result;
+    }
+
+    public static Polynomial multiplication(Polynomial x, Polynomial y) {
+        Polynomial result = new Polynomial();
+        for (int degX : x.monomials.keySet()) {
+            Double coeffX = x.monomials.get(degX);
+            for (int degY : y.monomials.keySet()) {
+                Double coeffY = y.monomials.get(degY);
+                int deg = degX + degY;
+                Double coeff = coeffX * coeffY;
+                result.addDouble(deg, coeff);
+            }
+        }
+        return result;
+    }
+
 
 }
