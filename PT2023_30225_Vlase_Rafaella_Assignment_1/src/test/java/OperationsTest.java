28c163450b98f11ca4923863d16db99cbaf9a745
Vlase Rafaella <kmyelly2@gmail.com>
2023-03-28 15:11:24 +0300

    doc

--- PT2023_30225_Vlase_Rafaella_Assignment_1/src/test/java/OperationsTest.java
+++ PT2023_30225_Vlase_Rafaella_Assignment_1/src/test/java/OperationsTest.java
@@ -16,7 +16,7 @@ public class OperationsTest {
     public void testAddition() {
         Polynomial p1 = new Polynomial("2x^2 - 3x + 1");
         Polynomial p2 = new Polynomial("-4x^2 + 5x - 2");
-        p1.polynomial2 = p2.polynomial;
+        p1.polynomial2 = p2.polynomial1;
         p1.addition();
         assertEquals("-2x^2 + 2x - 1", p1.toString());
     }
@@ -25,7 +25,7 @@ public class OperationsTest {
     public void testSubtraction() {
         Polynomial p1 = new Polynomial("2x^2 - 3x + 1");
         Polynomial p2 = new Polynomial("-4x^2 + 5x - 2");
-        p1.polynomial2 = p2.polynomial;
+        p1.polynomial2 = p2.polynomial1;
         p1.subtraction();
         assertEquals("6x^2 - 8x + 3", p1.toString());
     }
@@ -34,7 +34,7 @@ public class OperationsTest {
     public void testMultiplication() {
         Polynomial p1 = new Polynomial("2x^2 - 3x + 1");
         Polynomial p2 = new Polynomial("-4x + 2");
-        p1.polynomial2 = p2.polynomial;
+        p1.polynomial2 = p2.polynomial1;
         p1.multiplication();
         assertEquals("-8x^3 + 16x^2 - 8x", p1.toString());
     }
@@ -42,14 +42,14 @@ public class OperationsTest {
     @Test
     public void testDerivative() {
         Polynomial p1 = new Polynomial("3x^2 - 2x + 1");
-        p1.derivative(p1.polynomial);
+        p1.derivative(p1.polynomial1);
         assertEquals("6x - 2", p1.toString());
     }
 
     @Test
     public void testIntegration() {
         Polynomial p1 = new Polynomial("2x^2 - 3x + 1");
-        p1.integration(p1.polynomial);
+        p1.integration();
         assertEquals("0.667x^3 - 1.5x^2 + x", p1.toString());
     }
 
